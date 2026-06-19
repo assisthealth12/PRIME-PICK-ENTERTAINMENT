1d21de074f0c2e31b41f1c3437122337447a12e0
assisthealth12 <pannaga3254@gmail.com>
2026-06-19 15:00:43 +0530

    Revert visible production section to keep keywords hidden

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,13 +1,11 @@
 import { Hero } from "@/components/home/Hero";
 import { FeaturedFilms } from "@/components/home/FeaturedFilms";
-import { ProductionServices } from "@/components/home/ProductionServices";
 import { DirectorsHighlight } from "@/components/home/DirectorsHighlight";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
-      <ProductionServices />
       <FeaturedFilms />
       <DirectorsHighlight />
     </div>
